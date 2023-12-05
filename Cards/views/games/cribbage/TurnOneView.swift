ef665aae3bb16d83b144de54b82d680ea89f6c1a
Daniel Wells <112579766+dan424gg@users.noreply.github.com>
2023-12-05 12:40:00 -0800

    Crib deal UI is up for all player cases

--- Cards/views/games/cribbage/TurnOneView.swift
+++ Cards/views/games/cribbage/TurnOneView.swift
@@ -21,46 +21,47 @@ struct TurnOneView: View {
     
     var body: some View {
         VStack {
-            HStack(spacing: 10) {
-                if cardsDragged.count > 0 {
-                    CardView(cardItem: cardsDragged[0], cardIsDisabled: $cardIsDisabled)
-                } else {
-                    CardPlaceHolder()
-                        .border(firstDropAreaBorderColor, width: firstDropAreaBorderWidth)
-                        .dropDestination(for: CardItem.self) { items, location in
-                            if !cardsDragged.contains(items.first!) {
-                                cardsDragged.append(items.first!)
-                                cardsInHand.removeAll(where: { card in
-                                    card == items.first!
-                                })
-                            }
-                            return true
-                        } isTargeted: { inDropArea in
-                            firstDropAreaBorderColor = inDropArea ? .green : .clear
-                            firstDropAreaBorderWidth = inDropArea ? 7.0 : 1.0
-                        }
-                }
-                
-                if firebaseHelper.gameInfo?.num_teams ?? 2 == 2 {
-                    if cardsDragged.count > 1 {
-                        CardView(cardItem: cardsDragged[1], cardIsDisabled: $cardIsDisabled)
-                    } else {
+            switch (firebaseHelper.gameInfo?.num_players ?? 6) {
+            case 2:
+                HStack {
+                    if cardsDragged.count == 0 {
+                        Text("\(cardsDragged.count)")
                         CardPlaceHolder()
-                            .border(secondDropAreaBorderColor, width: secondDropAreaBorderWidth)
-                            .dropDestination(for: CardItem.self) { items, location in
-                                if !cardsDragged.contains(items.first!) {
-                                    cardsDragged.append(items.first!)
-                                    cardsInHand.removeAll(where: { card in
-                                        card == items.first!
-                                    })
-                                }
-                                return true
-                            } isTargeted: { inDropArea in
-                                secondDropAreaBorderColor = inDropArea ? .green : .clear
-                                secondDropAreaBorderWidth = inDropArea ? 7.0 : 1.0
-                            }
+                        CardPlaceHolder()
+                    } else {
+                        if cardsDragged.count == 1 {
+                            CardView(cardItem: cardsDragged[0], cardIsDisabled: .constant(true))
+                            CardPlaceHolder()
+                        } else {
+                            CardView(cardItem: cardsDragged[0], cardIsDisabled: .constant(true))
+                            CardView(cardItem: cardsDragged[1], cardIsDisabled: .constant(true))
+                        }
                     }
                 }
+            case 3,4:
+                if cardsDragged.count == 0 {
+                    CardPlaceHolder()
+                } else {
+                    CardView(cardItem: cardsDragged[0], cardIsDisabled: .constant(true))
+                }
+            case 6:
+                let dealer = firebaseHelper.players.first(where: { player in
+                    player.is_dealer!
+                })
+                
+                if (!(firebaseHelper.playerInfo?.is_dealer! ?? false))
+                    /* or if player is "to the left" of the dealer */
+                    || ((firebaseHelper.playerInfo?.player_num! ?? 1 + 1) % (firebaseHelper.gameInfo?.num_players ?? 2)) != (dealer?.player_num! ?? 1) {
+                    if cardsDragged.count == 0 {
+                        CardPlaceHolder()
+                    } else {
+                        CardView(cardItem: cardsDragged[0], cardIsDisabled: .constant(true))
+                    }
+                } else {
+                    Text("Waiting for players to discard...")
+                }
+            default:
+                Text("don't get here")
             }
         }
         .frame(height: 100)
