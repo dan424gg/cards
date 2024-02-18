1e419750845c1d56f490faa5440f67127d990afb
Daniel Wells <112579766+dan424gg@users.noreply.github.com>
2024-02-18 11:45:23 -0800

    finished the funcitonality!!

---
+++ Cards/ui/GameOutcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  GameOutcomeView.swift
+//  Cards
+//
+//  Created by Daniel Wells on 2/18/24.
+//
+
+import SwiftUI
+
+struct GameOutcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameOutcomeView()
+}
