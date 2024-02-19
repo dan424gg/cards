761f66752c28cd1e9751df295d5c811eec5f8bc3
Daniel Wells <112579766+dan424gg@users.noreply.github.com>
2024-02-19 02:16:20 -0800

    integrated a placeholder "cards" title

--- Cards/views/IntroView.swift
+++ Cards/views/IntroView.swift
@@ -16,31 +16,92 @@ struct IntroView: View {
     @State var offset: CGFloat = -50.0
     @State var heartPos: CGPoint = CGPoint(x: 0, y: 0)
     
-    var array: [Int] = Array(0...11)
+    var array: [Int] = Array(0...19)
     
     var body: some View {
         GeometryReader { geo in
             ZStack {
-                ForEach(array, id: \.self) { i in
-                    
-                    switch (i % 4) {
-                        case 0:
-                            SuitImage(suit: "diamond", index: CGFloat(i), geo: geo)
-                        case 1:
-                            SuitImage(suit: "club", index: CGFloat(i), geo: geo)
-                        case 2:
-                            SuitImage(suit: "heart", index: CGFloat(i), geo: geo)
-                        case 3:
-                            SuitImage(suit: "spade", index: CGFloat(i), geo: geo)
-                        default:
-                            EmptyView()
+                ZStack {
+                    ForEach(array, id: \.self) { i in
+                        switch (i % 4) {
+                            case 0:
+                                SuitImage(suit: "diamond", index: CGFloat(i), geo: geo)
+                            case 1:
+                                SuitImage(suit: "club", index: CGFloat(i), geo: geo)
+                            case 2:
+                                SuitImage(suit: "heart", index: CGFloat(i), geo: geo)
+                            case 3:
+                                SuitImage(suit: "spade", index: CGFloat(i), geo: geo)
+                            default:
+                                EmptyView()
+                        }
                     }
                 }
+                ZStack {
+                    ForEach (Array(1...9), id: \.self) { y in
+                        ZStack {
+                            ForEach(array, id: \.self) { i in
+                                switch ((i + y) % 4) {
+                                    case 0:
+                                        SuitImage(suit: "diamond", index: CGFloat(i), geo: geo)
+                                    case 1:
+                                        SuitImage(suit: "club", index: CGFloat(i), geo: geo)
+                                    case 2:
+                                        SuitImage(suit: "heart", index: CGFloat(i), geo: geo)
+                                    case 3:
+                                        SuitImage(suit: "spade", index: CGFloat(i), geo: geo)
+                                    default:
+                                        EmptyView()
+                                }
+                            }
+                        }
+                        .offset(y: CGFloat(-85 * y))
+                        
+                        ZStack {
+                            ForEach(array, id: \.self) { i in
+                                switch ((i + y) % 4) {
+                                    case 0:
+                                        SuitImage(suit: "diamond", index: CGFloat(i), geo: geo)
+                                    case 1:
+                                        SuitImage(suit: "club", index: CGFloat(i), geo: geo)
+                                    case 2:
+                                        SuitImage(suit: "heart", index: CGFloat(i), geo: geo)
+                                    case 3:
+                                        SuitImage(suit: "spade", index: CGFloat(i), geo: geo)
+                                    default:
+                                        EmptyView()
+                                }
+                            }
+                        }
+                        .offset(y: CGFloat(85 * y))
+                    }
+                }
+                Text("Cards")
+                    .font(.largeTitle)
+                    .shadow(color: .white, radius: 5)
+                    .shadow(color: .white, radius: 5)
+                    .shadow(color: .white, radius: 5)
+                    .shadow(color: .white, radius: 5)
+                    .shadow(color: .white, radius: 5)
+                    .position(x: geo.frame(in: .local).width / 2, y: geo.frame(in: .local).height / 3)
+                
+//                HStack {
+//                    NavigationLink {
+//                        NewGame()
+//                    } label: {
+//                        Text("New Game")
+//                    }
+//                    
+//                    NavigationLink {
+//                        ExistingGame()
+//                    } label: {
+//                        Text("Join Game")
+//                    }
+//                }
+//                .buttonStyle(.borderedProminent)
             }
         }
         .ignoresSafeArea()
-        .frame(width: 250, height: 250)
-        .border(.black)
     }
 }
 
@@ -54,23 +115,21 @@ struct SuitImage: View {
     var body: some View {
         GeometryReader { geoProxy in
             Image(systemName: "suit.\(suit)")
-                .foregroundColor(suit == "spade" || suit == "club" ? .black : .red)
-                .opacity(0.3)
+                .foregroundColor(suit == "spade" || suit == "club" ? .black.opacity(0.2) : .red.opacity(0.2))
                 .position(pos)
                 .onAppear {
                     let a = geo.frame(in: .local).maxX + 20.0
                     let b = geo.frame(in: .local).maxY + 20.0
-                    let c: CGFloat = sqrt((a*a) + (b*b))
                     
-                    pos = CGPoint(x: a - (index * (a / CGFloat(12))), y: (index * (b / CGFloat(12))) - 20.0)
-//                    withAnimation(.linear(duration: 15.0 * (1 - (index / 12)))) {
-//                        pos = CGPoint(x: -20.0, y: b)
-//                    } completion: {
-//                        pos = CGPoint(x: a, y: -20.0)
-//                        withAnimation(.linear(duration: 15.0).repeatForever(autoreverses: false)) {
-//                            pos = CGPoint(x: -20.0, y: b)
-//                        }
-//                    }
+                    pos = CGPoint(x: a - (index * (a / CGFloat(20))), y: (index * (b / CGFloat(20))) - 20.0)
+                    withAnimation(.linear(duration: 100.0 * (1 - (index / 20)))) {
+                        pos = CGPoint(x: -20.0, y: b)
+                    } completion: {
+                        pos = CGPoint(x: a, y: -20.0)
+                        withAnimation(.linear(duration: 100.0).repeatForever(autoreverses: false)) {
+                            pos = CGPoint(x: -20.0, y: b)
+                        }
+                    }
                 }
         }
         .ignoresSafeArea()
@@ -78,6 +137,5 @@ struct SuitImage: View {
 }
 
 #Preview {
-//    SuitImage(suit: "club")
     IntroView()
 }
