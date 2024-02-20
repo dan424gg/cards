392bf491e41a25e24bb214067126b4e4edb86708
Daniel Wells <112579766+dan424gg@users.noreply.github.com>
2024-02-20 15:21:46 -0800

    when the sheet is hidden, an opacity modifier is applied, all based on the height of the sheet

--- Cards/models/sheet items/SheetDisplayer.swift
+++ Cards/models/sheet items/SheetDisplayer.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct SheetDisplayer<Sheet: SheetEnum>: ViewModifier {
     @StateObject var coordinator: SheetCoordinator<Sheet>
     @State var detentSelected: PresentationDetent = .height(0.0)
+    @State var opacity: Double = 1.0
 
     func body(content: Content) -> some View {
         content
@@ -18,14 +19,30 @@ struct SheetDisplayer<Sheet: SheetEnum>: ViewModifier {
                 print("dismissed")
                 detentSelected = .fraction(0.02)
             }, content: { sheet in
-                sheet
-                    .view(coordinator: coordinator)
-                    .onAppear {
-                        detentSelected = sheet.detents.first!
-                    }
-                    .presentationCornerRadius(57.0)
-                    .presentationDetents(Set(sheet.detents), selection: $detentSelected)
-                    .interactiveDismissDisabled()
+                GeometryReader { geo in
+                    sheet
+                        .view(coordinator: coordinator)
+                        .padding()
+                        .opacity(opacity)
+                        .onAppear {
+                            detentSelected = sheet.detents.first!
+                        }
+                        .presentationCornerRadius(45.0)
+                        .presentationDetents(Set(sheet.detents), selection: $detentSelected)
+                        .interactiveDismissDisabled()
+                        .onChange(of: geo.frame(in: .local).height, { 
+                            if (geo.frame(in: .global).height / geo.frame(in: .global).maxY <= 0.1) {
+                                withAnimation(.easeInOut(duration: 0.2)) {
+                                    opacity = ((((geo.frame(in: .global).height / geo.frame(in: .global).maxY) - 0.02) / 0.8) * 10)
+                                    print(opacity)
+                                }
+                            } else {
+                                withAnimation(.easeInOut(duration: 0.2)) {
+                                    opacity = 1.0
+                                }
+                            }
+                        })
+                }
             })
     }
 }
